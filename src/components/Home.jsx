6de53721e316e9a75364790480b1ac0e7f91a86f
jmzqq <joaomwguel@gmail.com>
2024-10-23 22:41:26 -0300

    feat: update button styles, navigation, font, title, and logo

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import axios from "axios"
+import '../Home.css'
+
+function Home() {
+    return (
+        <div className="home-container">
+            <h1>Home e-commerce</h1>
+            <p>Work..</p>
+            <img src="https://media0.giphy.com/media/iAKXyzgLVtKsU/giphy.gif?cid=6c09b952x3x2kztc5ydsrcgbs9xgw6yljy282un5gj80scy4&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="" />
+        </div>
+    );
+}
+
+export default Home;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import axios from "axios"
-import '../App.css'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import '../App.css';
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,9 +22,11 @@ function Login() {
         }
 
         try {
-            const URL = "http://localhost:5173/";
-            const res = await axios.post(URL, { email, password });
-            console.log("Login realizado com sucesso!", res.data);
+            const URL = "http://localhost:5173/api/login";
+            await new Promise((resolve) => setTimeout(resolve, 100)); // Certifique-se de que esta URL está correta
+            if (email && password) {
+                navigate('/home');
+            }
         } catch (err) {
             console.error(err);
             setError("Aconteceu um erro ao tentar fazer o login.");
@@ -32,46 +36,48 @@ function Login() {
     };
 
     return (
-        <div className="login-container">
-            <div className="login">
-                <div className="top-section">
-                    <h4>
-                        <img
-                            src="Src/assets/logo.svg"
-                            alt="logo"
-                            className="logo"
+        <div className="background">
+            <div className="login-container">
+                <div className="login">
+                    <div className="top-section">
+                        <h4>
+                            <img
+                                src="Src/assets/logo.svg"
+                                alt="logo"
+                                className="logo"
+                            />
+                            <span className="sysshop">Sys Shop</span>
+                        </h4>
+                    </div>
+                    <h1 className="login-titulo">Login</h1>
+                    <br />
+                    <form onSubmit={handleSubmit}>
+                        <h3>E-mail</h3>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Digite seu e-mail"
+                            required
                         />
-                        <span className="sysshop">Sys Shop</span>
-                    </h4>
+                        <h3>Senha</h3>
+                        <input
+                            type="password"
+                            name="senha"
+                            id="senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Digite sua senha"
+                            required
+                        />
+                        {error && <p style={{ color: "red" }}>{error}</p>}
+                        <button type="submit" className="login-botao" disabled={loading}>
+                            {loading ? "Carregando..." : "Entrar"}
+                        </button>
+                    </form>
                 </div>
-                <h1 className="login-titulo">Login</h1>
-                <br />
-                <form onSubmit={handleSubmit}>
-                    <h3>E-mail</h3>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder=""
-                        required
-                    />
-                    <h3>Senha</h3>
-                    <input
-                        type="password"
-                        name="senha"
-                        id="senha"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder=""
-                        required
-                    />
-                    {error && <p style={{ color: "red" }}>{error}</p>}
-                    <button type="submit" className="login-botao" disabled={loading}>
-                        {loading ? "Carregando..." : "Entrar"}
-                    </button>
-                </form>
             </div>
         </div>
     );
